46ee0f80541ef7182147544065dc09c58dbc443f
Mark Senff <mark@senff.com>
2020-03-29 15:50:41 -0400

    Whoops.

--- zendesk-check-open-tickets.js
+++ zendesk-check-open-tickets.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Zendesk Check Open Tickets
 // @namespace    http://tampermonkey.net/
-// @version      0.97 rev. 230676
+// @version      0.98 rev. 230676
 // @description  Checks if this user has OPEN/PENDING/ON-HOLD/NEW tickets and will display a notification at the top if they do
 // @author       Senff
 // @updateURL    https://github.com/senff/Zendesk-markdown-buttons/raw/master/zendesk-markdown-buttons.user.js
@@ -40,7 +40,7 @@ function checkTickets() {
                 });
                 if(winkWink > 0) {
                     $('body').addClass('saynoMORE');
-                    nudgeNudge = "<div class='nudgeNudge'><span style='font-size:18px;'>⚠️</span> <span class='whatsItLike'><strong>THIS USER HAS OTHER OPEN/PENDING TICKETS!</strong> Please check those first before you reply here.</span>&nbsp;&nbsp;&nbsp;<a href='#'>Cool, got it.</a></strong></div>";
+                    nudgeNudge = "<div class='nudgeNudge'><span style='font-size:18px;'>⚠️</span> <span class='whatsItLike'><strong>THIS USER HAS OTHER OPEN/PENDING TICKETS!</strong> Please check those first before you reply here.</span>&nbsp;&nbsp;&nbsp;<span class='cool'>Cool, got it.</span></strong></div>";
                     $(nudgeNudge).prependTo('body');
                     var nudgeHeight = $('.nudgeNudge').outerHeight()+5;
                     $('body').css('padding-top',nudgeHeight+'px');
@@ -59,13 +59,13 @@ function blinkieBlink() {
   }, 800);
 }
 
-$('body').on('click','.nudgeNudge a',function(){
-    $('.nudgeNudge a').html('Baiiiiiiiiii');
+$('body').on('click','.nudgeNudge .cool',function(){
+    $('.nudgeNudge .cool').html('Baiiiiiiiiii');
     $('.nudgeNudge').fadeOut(500);
     $('body').removeClass('saynoMORE').animate({padding: 0}, 800, function() {});
 });
 
 $(document).ready(function() {
-    $('body').append('<style type="text/css" id="nudgestyles">.nudgeNudge{box-sizing:border-box; position: fixed; width: 100%; top:0; z-index: 100; background: #aa0000; color: #ffffff; padding: 7px 10px 10px; line-height: 20px;}.nudgeNudge a {color: #FFFFCC;font-weight: bold; text-decoration:underline;}.hidethis{opacity:0;}.this-ticket td{background: #e7e7e7 !important;filter: grayscale(100%);}.this-ticket td:first-child {border-left: solid 1px #cccccc;}.this-ticket td.subject a,.this-ticket td.when {cursor: default;pointer-events: none;color: #888888 !important;text-shadow: 1px 1px 0 #ffffff;font-style: italic;font-size: 12px;}</style>');
+    $('body').append('<style type="text/css" id="nudgestyles">.nudgeNudge{box-sizing:border-box; position: fixed; width: 100%; top:0; z-index: 100; background: #aa0000; color: #ffffff; padding: 7px 10px 10px; line-height: 20px;}.nudgeNudge .cool{color: #FFFFCC;font-weight: bold; text-decoration:underline; cursor: pointer;}.hidethis{opacity:0;}.this-ticket td{background: #e7e7e7 !important;filter: grayscale(100%);}.this-ticket td:first-child {border-left: solid 1px #cccccc;}.this-ticket td.subject a,.this-ticket td.when {cursor: default;pointer-events: none;color: #888888 !important;text-shadow: 1px 1px 0 #ffffff;font-style: italic;font-size: 12px;}</style>');
     checkTickets();
 });
